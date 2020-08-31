1005bc22f33721c527cf72b65e23ac64b8a7d8f0
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2020-08-31 11:46:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -67,16 +67,11 @@ function draw() {
 }
 
 function keyPressed() {
-	if(keyCode === DOWN_ARROW){
-	packageSprite.VelocityY = 2; 
-	}
- if (keyCode === LEFT_ARROW) { helicopterSprite.x=helicopterSprite.x-20; 
-			      translation={x:-20,y:0} Matter.Body.translate(packageBody, translation) } 
-	else if (keyCode === RIGHT_ARROW) 
-	{ helicopterSprite.x=helicopterSprite.x+20; 
+	if (keyCode === LEFT_ARROW) { helicopterSprite.x=helicopterSprite.x-20; translation={x:-20,y:0}
+         Matter.Body.translate(packageBody, translation) } 
+	 else if (keyCode === RIGHT_ARROW) { helicopterSprite.x=helicopterSprite.x+20; 
 	 translation={x:20,y:0} Matter.Body.translate(packageBody, translation) }
-  }
-
-
-
+         else if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false);
+ } 
+}
 
